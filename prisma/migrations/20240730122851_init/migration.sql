3b260545b4a3dc66d0f1a1723cc4e79fa767c3b1
Ons Khiari <98515359+ons-khiari@users.noreply.github.com>
2024-08-07 09:38:55 +0100

    proposal fixes

---
+++ prisma/migrations/20240730122851_init/migration.sql
@@ -0,0 +1,235 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('CONDIDATE', 'EMPLOYER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "JobStatus" AS ENUM ('AVAILABLE', 'DONE');
+
+-- CreateEnum
+CREATE TYPE "ProposalStatus" AS ENUM ('ACCEPTED', 'DECLINED', 'PENDING');
+
+-- CreateEnum
+CREATE TYPE "JobType" AS ENUM ('FULL_TIME', 'PART_TIME', 'CONTRACT', 'INTERNSHIP', 'REMOTE');
+
+-- CreateEnum
+CREATE TYPE "ExperienceLevel" AS ENUM ('ENTRY', 'MID', 'SENIOR');
+
+-- CreateEnum
+CREATE TYPE "ProjectStatus" AS ENUM ('SOLD', 'AVAILABLE');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'CONDIDATE',
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "regstrationDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "firstName" TEXT NOT NULL DEFAULT '',
+    "lastName" TEXT NOT NULL DEFAULT '',
+    "phoneNumber" TEXT NOT NULL DEFAULT '',
+    "address" TEXT NOT NULL DEFAULT '',
+    "city" TEXT NOT NULL DEFAULT '',
+    "state" TEXT NOT NULL DEFAULT '',
+    "country" TEXT NOT NULL DEFAULT '',
+    "avatar" TEXT NOT NULL DEFAULT '',
+    "bio" TEXT NOT NULL DEFAULT '',
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Skills" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Skills_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SkillsProfile" (
+    "profileId" INTEGER NOT NULL,
+    "skillId" INTEGER NOT NULL,
+
+    CONSTRAINT "SkillsProfile_pkey" PRIMARY KEY ("skillId","profileId")
+);
+
+-- CreateTable
+CREATE TABLE "SkillsJob" (
+    "jobId" INTEGER NOT NULL,
+    "skillId" INTEGER NOT NULL,
+
+    CONSTRAINT "SkillsJob_pkey" PRIMARY KEY ("skillId","jobId")
+);
+
+-- CreateTable
+CREATE TABLE "ProfileRatings" (
+    "id" SERIAL NOT NULL,
+    "profileId" INTEGER NOT NULL,
+    "raterProfileId" INTEGER NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "comment" TEXT NOT NULL,
+
+    CONSTRAINT "ProfileRatings_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Experience" (
+    "id" SERIAL NOT NULL,
+    "profileId" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "company" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Experience_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Education" (
+    "id" SERIAL NOT NULL,
+    "profileId" INTEGER NOT NULL,
+    "school" TEXT NOT NULL,
+    "degree" TEXT NOT NULL,
+    "fieldOfStudy" TEXT NOT NULL,
+    "startYear" INTEGER NOT NULL,
+    "endYear" INTEGER NOT NULL,
+
+    CONSTRAINT "Education_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Social" (
+    "id" SERIAL NOT NULL,
+    "profileId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+
+    CONSTRAINT "Social_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Job" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "budgetmin" DOUBLE PRECISION NOT NULL,
+    "budgetmax" DOUBLE PRECISION NOT NULL,
+    "location" TEXT NOT NULL,
+    "jobType" "JobType" NOT NULL,
+    "experienceLevel" "ExperienceLevel" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "postedById" INTEGER NOT NULL,
+    "jobStatus" "JobStatus" NOT NULL DEFAULT 'AVAILABLE',
+    "images" TEXT[],
+
+    CONSTRAINT "Job_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Proposal" (
+    "id" SERIAL NOT NULL,
+    "jobId" INTEGER NOT NULL,
+    "profileId" INTEGER NOT NULL,
+    "coverLetter" TEXT NOT NULL,
+    "budgetmin" DOUBLE PRECISION NOT NULL,
+    "budgetmax" DOUBLE PRECISION NOT NULL,
+    "days" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "projectId" INTEGER,
+    "proposalStatus" "ProposalStatus" NOT NULL DEFAULT 'PENDING',
+
+    CONSTRAINT "Proposal_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "location" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "soldAt" TIMESTAMP(3),
+    "status" "ProjectStatus" NOT NULL,
+    "postedById" INTEGER NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+    "images" TEXT[],
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CategoryProject" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "CategoryProject_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ProfileRatings_profileId_raterProfileId_key" ON "ProfileRatings"("profileId", "raterProfileId");
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SkillsProfile" ADD CONSTRAINT "SkillsProfile_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SkillsProfile" ADD CONSTRAINT "SkillsProfile_skillId_fkey" FOREIGN KEY ("skillId") REFERENCES "Skills"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SkillsJob" ADD CONSTRAINT "SkillsJob_jobId_fkey" FOREIGN KEY ("jobId") REFERENCES "Job"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SkillsJob" ADD CONSTRAINT "SkillsJob_skillId_fkey" FOREIGN KEY ("skillId") REFERENCES "Skills"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProfileRatings" ADD CONSTRAINT "ProfileRatings_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProfileRatings" ADD CONSTRAINT "ProfileRatings_raterProfileId_fkey" FOREIGN KEY ("raterProfileId") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Experience" ADD CONSTRAINT "Experience_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Education" ADD CONSTRAINT "Education_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Social" ADD CONSTRAINT "Social_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Job" ADD CONSTRAINT "Job_postedById_fkey" FOREIGN KEY ("postedById") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Proposal" ADD CONSTRAINT "Proposal_jobId_fkey" FOREIGN KEY ("jobId") REFERENCES "Job"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Proposal" ADD CONSTRAINT "Proposal_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_postedById_fkey" FOREIGN KEY ("postedById") REFERENCES "Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "CategoryProject"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
